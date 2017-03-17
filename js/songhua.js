d3e3aa6c6c11ebed218faef484fc0ba478d14e5d
yangyahui <1301959842@qq.com>
2017-03-17 14:53:20 +0800

    献花完了

---
+++ js/songhua.js
@@ -0,0 +1,39 @@
+ var piaoluo =  function(){
+
+  var d="<div class='snow' ><div>"
+
+			setInterval(function(){
+
+				var f=$(document).width();
+
+				var e=Math.random()*f-20;
+
+				var o=0.3+Math.random();
+
+				var fon=10+Math.random()*30;
+
+				var l = e - 100 + 200 * Math.random();
+
+				var k=2000 + 5000 * Math.random();
+
+				$(d).clone().appendTo(".snowbg").css({
+
+					left:e+"px",
+
+					opacity:o,
+
+					"font-size":fon,
+
+				}).animate({
+
+				  top:"400px",
+
+					left:l+"px",
+
+					opacity:0.1,
+
+				},k,"linear",function(){$(this).remove()})
+
+			},200)
+
+      };
