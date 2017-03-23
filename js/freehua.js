b4a92d67c31de3367b3eda1dd1845dca66d2982e
yangyahui <1301959842@qq.com>
2017-03-23 11:24:44 +0800

    改功德

--- js/freehua.js
+++ js/freehua.js
@@ -11,19 +11,19 @@ $(document).ready(function () {
                var html ='';
                html = "<div id='tanchuang'>"+ json.msg + '<div>';
                $('body').append(html);
-                if (json.code) {
+                if (!json.code) {
                     piaoluo();
                     $("#tanchuang").css("display","block");
                     $("#boxbg").css("display","block");
                     setTimeout(function () {
                         $("#tanchuang").css("display","none");
-                    }, 2000)
+                    }, 2000);
 
                 } else { 
                     $("#tanchuang").css("display","block").html('今天已送过');
                      setTimeout(function () {
                         $("#tanchuang").css("display","none");
-                    }, 2000)                       
+                    }, 2000);                       
                 }
             }
         });
